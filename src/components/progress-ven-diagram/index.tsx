fb2de2084a02a7c5f52da202a4b76c3e262e6a9b
Thomas Andrus <thomasmandrus@gmail.com>
2023-11-19 14:32:06 -0600

    started styling and added styled components

--- src/components/progress-ven-diagram/index.tsx
+++ src/components/progress-ven-diagram/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import "./progress-ven-diagram.css";
 
 export const ProgressVenDiagram = () => {
-    return <div>
-        something
-    </div>
+    return (
+      <div className="progress-ven-diagram">
+        {["first", "second"].map((circle) => (
+          <span
+            className={`progress-ven-diagram__circle progress-ven-diagram__circle--${circle}`}
+          ></span>
+        ))}
+      </div>
+    );
 }
