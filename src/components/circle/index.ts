1b14491d47ed7909e0f6cc17950608aa04921285
Thomas Andrus <thomasmandrus@gmail.com>
2023-11-21 06:22:11 -0600

    added some good animations

--- src/components/circle/index.ts
+++ src/components/circle/index.ts
@@ -8,7 +8,7 @@ export const Circle = styled.span<{
   position?: string;
   top?: string;
   left?: string;
-  percentage: number;
+  $percentage: number;
   z?: number;
 }>`
   border-radius: 50%;
@@ -20,8 +20,8 @@ export const Circle = styled.span<{
       width: ${props.size ? props.size : `${size}rem`};
       position: ${props.position ? props.position : "absolute"};
       background: conic-gradient(
-        rgba(0, 0, 0, 0) 0% ${100 - props.percentage}%,
-        ${props.color} ${100 - props.percentage}% ${props.percentage}%
+        rgba(0, 0, 0, 0) 0% ${100 - props.$percentage}%,
+        ${props.color} ${100 - props.$percentage}% ${props.$percentage}%
       );
       ${props.z ? `z-index: ${props.z};` : ""}
       transition: 
--- src/components/progress-ven-diagram/index.tsx
+++ src/components/progress-ven-diagram/index.tsx
@@ -1,11 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./progress-ven-diagram.css";
 import { Circle, size } from "../circle";
 import { ProgressVenDiagramProps as Props } from "./model";
-import {
-  calcPercentageOfMoneyLeft,
-  calcDayDifference,
-} from "./utils";
+import { calcPercentageOfMoneyLeft, calcDayDifference } from "./utils";
 
 export const ProgressVenDiagram = ({
   expenses,
@@ -14,44 +11,59 @@ export const ProgressVenDiagram = ({
   currentDate,
   dateOfNextPaycheck,
 }: Props) => {
+  const [centered, setCentered] = useState(true);
   const totalDays = calcDayDifference(startDate, dateOfNextPaycheck);
   const daysLeft = calcDayDifference(currentDate, dateOfNextPaycheck);
   const percentageOfDaysLeft = daysLeft / totalDays;
   const percentageOfMoneyLeft = calcPercentageOfMoneyLeft(paycheck, expenses);
   const moneyPercentageLarger =
     percentageOfMoneyLeft - percentageOfDaysLeft >= 0;
-  const moneyColor = 'green';
-  const timeRemainingColor = moneyPercentageLarger ? 'limeGreen' : 'red';
+  const moneyColor = "green";
+  const timeRemainingColor = moneyPercentageLarger ? "limeGreen" : "red";
 
   return (
     <div className="progress-ven-diagram">
-      {[
-        {
-          circle: "first",
-          color: moneyColor,
-          circleSize: `${size}rem`,
-          position: "",
-          percent: percentageOfMoneyLeft,
-          z: moneyPercentageLarger ? 0 : 1,
-        },
-        {
-          circle: "second",
-          color: timeRemainingColor,
-          circleSize: `${size}rem`,
-          percent: percentageOfDaysLeft,
-          z: moneyPercentageLarger ? 1 : 0,
-        },
-      ].map(({ circle, color, circleSize, percent, z }) => (
-        <Circle
-          key={`${circle}_circle-key`}
-          className={`progress-ven-diagram__circle progress-ven-diagram__circle--${circle}`}
-          color={color}
-          size={circleSize}
-          percentage={percent * 100}
-          z={z}
-        />
-      ))}
-      
+      <button
+        onClick={() => setCentered((prev) => !prev)}
+        className="progress-ven-diagram__wrapper"
+      >
+        {[
+          {
+            circle: "first",
+            color: moneyColor,
+            circleSize: `${size}rem`,
+            position: "",
+            percent: percentageOfMoneyLeft,
+            z: moneyPercentageLarger ? 0 : 1,
+          },
+          {
+            circle: "second",
+            color: timeRemainingColor,
+            circleSize: `${size}rem`,
+            percent: percentageOfDaysLeft,
+            z: moneyPercentageLarger ? 1 : 0,
+          },
+        ].map(({ circle, color, circleSize, percent, z }) => (
+          <Circle
+            key={`${circle}_circle-key`}
+            className={`progress-ven-diagram__circle progress-ven-diagram__circle--${circle} ${
+              centered ? `progress-ven-diagram__circle--${circle}-centered` : ""
+            }`}
+            color={color}
+            size={circleSize}
+            $percentage={percent * 100}
+            z={z}
+          />
+        ))}
+      </button>
+      <div>
+        <p>
+          Days {daysLeft}/{totalDays} till next paycheck
+        </p>
+        <p>
+          Money ${expenses}/${paycheck}
+        </p>
+      </div>
     </div>
   );
 };
