0fa1eaf11f96ef61c3fb6fe8826bc68c71a640b3
Thomas Andrus <thomasmandrus@gmail.com>
2023-12-16 10:08:38 -0600

    form is set up

--- src/components/manual-form/index.tsx
+++ src/components/manual-form/index.tsx
@@ -1,19 +1,25 @@
-import React, { useReducer } from "react";
-import { Form, InputType, Props } from "./model";
+import React, { useReducer, ChangeEvent } from "react";
+import { Form, InputType, Props, State } from "./model";
 import { CurrencyInput } from "../currancy-input";
 import { reducer } from "./utils";
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
-const initial: Form = {
-  dateOfNextPaycheck: new Date(),
-  startDate: new Date(),
+const initial: State = {
+  dateOfNextPaycheck: "",
+  startDate: "",
   expenses: 0,
   paycheck: 0,
 };
 
 export const ManualForm = ({ submit }: Props) => {
   const [form, dispatch] = useReducer(reducer, initial);
-  const setMoney = (type: InputType.EXPENSES | InputType.PAYCHECK) => (value: number) => dispatch({ type, value})
+  const setMoney =
+    (type: InputType.EXPENSES | InputType.PAYCHECK) => (value: number) =>
+      dispatch({ type, value });
+  const setDate =
+    (type: InputType.START_DATE | InputType.DATE_OF_NEXT_PAYCHECK) =>
+    (event: ChangeEvent<HTMLInputElement>) =>
+      dispatch({ type, value: event.target.value });
   return (
     <div className={styles.form}>
       <label>
@@ -29,6 +35,22 @@ export const ManualForm = ({ submit }: Props) => {
           value={form.expenses as number}
           onChange={setMoney(InputType.EXPENSES)}
         ></CurrencyInput>
+      </label>
+      <label>
+        Start:
+        <input
+          type="date"
+          value={form.startDate}
+          onChange={setDate(InputType.START_DATE)}
+        ></input>
+      </label>
+      <label>
+        Next Paycheck:
+        <input
+          type="date"
+          value={form[InputType.DATE_OF_NEXT_PAYCHECK]}
+          onChange={setDate(InputType.DATE_OF_NEXT_PAYCHECK)}
+        ></input>
       </label>
     </div>
   );
--- src/components/manual-form/model.ts
+++ src/components/manual-form/model.ts
@@ -1,9 +1,9 @@
-import { ProgressVenDiagramProps } from '../progress-ven-diagram/model';
+import { ProgressVenDiagramProps } from "../progress-ven-diagram/model";
 
-export type Form = Omit<ProgressVenDiagramProps, 'currentDate'>;
+export type Form = Omit<ProgressVenDiagramProps, "currentDate">;
 
 export interface Props {
-    submit: (form:Form) => void;
+  submit: (form: Form) => void;
 }
 
 export enum InputType {
@@ -13,14 +13,21 @@ export enum InputType {
   START_DATE = "startDate",
 }
 
+export interface State {
+  [InputType.EXPENSES]: number;
+  [InputType.PAYCHECK]: number;
+  [InputType.START_DATE]: string;
+  [InputType.DATE_OF_NEXT_PAYCHECK]: string;
+}
+
 export interface MoneyAction {
-    type: InputType.PAYCHECK | InputType.EXPENSES;
-    value: number;
+  type: InputType.PAYCHECK | InputType.EXPENSES;
+  value: number;
 }
 
 export interface DateAction {
-    type: InputType.DATE_OF_NEXT_PAYCHECK | InputType.START_DATE;
-    value: number;
+  type: InputType.DATE_OF_NEXT_PAYCHECK | InputType.START_DATE;
+  value: string;
 }
 
 export type Action = MoneyAction | DateAction;
